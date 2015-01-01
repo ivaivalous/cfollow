2f3b74f4f56d161aa68c416f34c1f48c2df00545
Ivaylo Marinkov <git@tsarstva.bg>
2015-01-01 23:39:05 +0200

    Updated client build script

--- build-scripts/client.sh
+++
@@ -1,4 +0,0 @@
-cd ..
-mkdir -p client/schema
-\cp configuration-schema/*.json client/schema
-\cp communication-schema/*.json client/schema
---
+++ build/client.sh
@@ -0,0 +1,6 @@
+cd ..
+mkdir -p client/schema
+mkdir -p clint/logs
+\cp schema/configuration/*.json client/schema
+\cp schema/communication/*.json client/schema
+\cp schema/logging/*.json client/schema
