c68abc75d6d0e46c3112f7f69cc50c790178aacb
Ivaylo Marinkov <git@tsarstva.bg>
2015-01-02 00:06:12 +0200

    Fixed typo

--- build/client.sh
+++ build/client.sh
@@ -1,6 +1,6 @@
 cd ..
 mkdir -p client/schema
-mkdir -p clint/logs
+mkdir -p client/logs
 \cp schema/configuration/*.json client/schema
 \cp schema/communication/*.json client/schema
 \cp schema/logging/*.json client/schema
