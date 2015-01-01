5a8ebee63cf9f10a18c875b045af3b98e5dcd48c
Ivaylo Marinkov <git@tsarstva.bg>
2015-01-02 00:39:30 +0200

    Updated build script to allow passing the API key

--- build/client.sh
+++ build/client.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+# Build the cfollow client
+# Add API key as the first parameter
 cd ..
 mkdir -p client/schema
 mkdir -p client/logs
 \cp schema/configuration/*.json client/schema
 \cp schema/communication/*.json client/schema
 \cp schema/logging/*.json client/schema
+sed -i 's/___API_KEY___/$1/g' client/config.json
